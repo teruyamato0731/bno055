24730e8466d21a261a137c1d1e7c40fac93a3c98
Yoshikawa Teru <77840326+teruyamato0731@users.noreply.github.com>
2023-10-11 15:02:22 +0900

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <mbed.h>
+
+// Const variable
+
+// Function prototype
+
+// IO
+// CAN can{PA_11, PA_12, (int)1e6};
+// CAN can{PB_12, PB_13, (int)1e6};
+// CANMessage msg;
+Timer timer;
+
+// Struct definition
+
+// Global variable
+
+/// @brief The application entry point.
+int main() {
+  // put your setup code here, to run once:
+  printf("\nsetup\n");
+  timer.start();
+  auto pre = timer.elapsed_time();
+  while(1) {
+    // put your main code here, to run repeatedly:
+    auto now = timer.elapsed_time();
+    if(now - pre > 20ms) {
+      printf("hoge\n");
+      pre = now;
+    }
+  }
+}
+
+// Function definition
